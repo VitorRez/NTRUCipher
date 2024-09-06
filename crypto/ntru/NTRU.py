68ddb6249c1cd97f00c2e7148f87d9097004d610
VitorRez <vitorez2002@gmail.com>
2024-09-06 02:55:35 -0300

    avanço considerável na implementação do NSS, contudo, ainda não funciona plenamente

--- crypto/ntru/NTRU.py
+++ crypto/ntru/NTRU.py
@@ -7,16 +7,17 @@ from Crypto.Hash import SHA256
 import numpy as np
 import math
 
-def generate(N, p, q):
-    ntru = NtruCipher(N, p, q)
+def generate(N, p, q, Dmin, Dmax):
+    ntru = NtruCipher(N, p, q, Dmin, Dmax)
     ntru.generate_random_keys()
     
+    g = np.array(ntru.g_poly.all_coeffs()[::-1])
     h = np.array(ntru.h_poly.all_coeffs()[::-1])
     f = np.array(ntru.f_poly.all_coeffs()[::-1])
     f_p = np.array(ntru.f_p_poly.all_coeffs()[::-1])
     
-    priv_key = {'N': N, 'p': p, 'q': q, 'f': f, 'f_p': f_p}
-    pub_key = {'N': N, 'p': p, 'q': q, 'h': h}
+    priv_key = {'N': N, 'p': p, 'q': q, 'f': f, 'f_p': f_p, 'g': g, 'Dmin': Dmin, 'Dmax': Dmax}
+    pub_key = {'N': N, 'p': p, 'q': q, 'h': h, 'Dmin': Dmin, 'Dmax': Dmax}
     
     return priv_key, pub_key
 
@@ -24,7 +25,7 @@ def encrypt(pub_key, input_str):
 
     input_arr = np.unpackbits(np.frombuffer(input_str, dtype=np.uint8))
 
-    ntru = NtruCipher(int(pub_key['N']), int(pub_key['p']), int(pub_key['q']))
+    ntru = NtruCipher(int(pub_key['N']), int(pub_key['p']), int(pub_key['q']), int(pub_key['Dmin']), int(pub_key['Dmax']))
     ntru.h_poly = Poly(pub_key['h'].astype(int)[::-1], x).set_domain(ZZ)
 
     if ntru.N < len(input_arr):
@@ -38,7 +39,7 @@ def decrypt(priv_key, input):
 
     input_arr = np.array(input).flatten()
 
-    ntru = NtruCipher(int(priv_key['N']), int(priv_key['p']), int(priv_key['q']))
+    ntru = NtruCipher(int(priv_key['N']), int(priv_key['p']), int(priv_key['q']), int(priv_key['Dmin']), int(priv_key['Dmax']))
     ntru.f_poly = Poly(priv_key['f'].astype(int)[::-1], x).set_domain(ZZ)
     ntru.f_p_poly = Poly(priv_key['f_p'].astype(int)[::-1], x).set_domain(ZZ)
 
@@ -48,3 +49,30 @@ def decrypt(priv_key, input):
     decrypted = ntru.decrypt(Poly(input_arr[::-1], x).set_domain(ZZ)).all_coeffs()[::-1]
 
     return(np.packbits(np.array(decrypted).astype(int)).tobytes())
+
+def sign(priv_key, input_str):
+
+    input_arr = np.unpackbits(np.frombuffer(input_str, dtype=np.uint8))
+
+    ntru = NtruCipher(int(priv_key['N']), int(priv_key['p']), int(priv_key['q']), int(priv_key['Dmin']), int(priv_key['Dmax']))
+    ntru.f_poly = Poly(priv_key['f'].astype(int)[::-1], x).set_domain(ZZ)
+    ntru.f_p_poly = Poly(priv_key['f_p'].astype(int)[::-1], x).set_domain(ZZ)
+    ntru.g_poly = Poly(priv_key['g'].astype(int)[::-1], x).set_domain(ZZ)
+
+    if ntru.N < len(input_arr):
+        raise Exception("Input is too large for current N")
+    
+    try:
+        m_poly, s = ntru.sign(input_arr)
+        return m_poly, s
+    except:
+        print("couldn't sign")
+
+def verify(pub_key, m_poly, s):
+
+    ntru = NtruCipher(int(pub_key['N']), int(pub_key['p']), int(pub_key['q']), int(pub_key['Dmin']), int(pub_key['Dmax']))
+    ntru.h_poly = Poly(pub_key['h'].astype(int)[::-1], x).set_domain(ZZ)
+    
+    return ntru.verify(m_poly, s)
+    
+
--- teste.py
+++ teste.py
@@ -2,14 +2,45 @@ from crypto.ciphers import *
 from crypto.ntru.NTRU import *
 from crypto.ntru.ntrucipher import *
 
-priv_key_ntru, pub_key_ntru = generate(N=251, p=3, q=128)
-aes_key = get_random_bytes(16)
+priv_key_ntru, pub_key_ntru = generate(N=251, p=3, q=128, Dmin=55, Dmax=87)
+
+#aes_key = get_random_bytes(16)
 
 msg = b'banana'
+msg2 = b'laranja'
 
-e = CipherHandler(aes_key, pub_key_ntru)
-enc_text = e.e_protocol(msg)
-print(enc_text)
+m_poly1, s1 = sign(priv_key_ntru, msg)
+m_poly2, s2 = sign(priv_key_ntru, msg2)
+print(verify(pub_key_ntru, m_poly1, s1))
+print(verify(pub_key_ntru, m_poly1, s2))
+print(verify(pub_key_ntru, m_poly2, s1))
+print(verify(pub_key_ntru, m_poly2, s2))
 
-plaintext = e.d_protocol(enc_text, priv_key_ntru)
-print(plaintext)
+#output = encrypt(pub_key_ntru, msg)
+
+#print(output)
+
+#clear_msg = decrypt(priv_key_ntru, output)
+
+#clear_msg = [(x + 256) % 256 for x in clear_msg]
+
+#print(clear_msg)
+
+# Convert the list of integers to a byte array
+#byte_data = bytes(clear_msg)
+
+#try:
+#    # Try to decode the byte data back to a string (if it contains textual data)
+#    decoded_msg = byte_data.decode('utf-8')
+#    print("Decrypted:", decoded_msg)
+#except UnicodeDecodeError:
+#    # If it's not a valid UTF-8 string, print the byte data instead
+#    print("Decrypted message cannot be decoded as UTF-8 string.")
+#    print("Byte data:", byte_data)
+
+#e = CipherHandler(aes_key, pub_key_ntru)
+#enc_text = e.e_protocol(msg)
+#print(enc_text)
+
+#plaintext = e.d_protocol(enc_text, priv_key_ntru)
+#print(plaintext)

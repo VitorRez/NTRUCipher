43386547fa24caf4b1fff15b9010d4b9a89229a6
VitorRez <vitorez2002@gmail.com>
2024-10-18 18:54:29 -0300

    pequenas mudanças na função para calcular hash

--- crypto/hash.py
+++ crypto/hash.py
@@ -1,8 +1,8 @@
 from Crypto.Hash import SHA256
 
 def create_hash(text):
-    t_hash = SHA256.new(bytes(text,'utf-8'))
-    return t_hash.digest()
+    thash = SHA256.new(bytes(text,'utf-8'))
+    return thash.digest()
 
 def verify_hash(text, t_hash):
     t_hash1 = SHA256.new(bytes(text,'utf-8'))
